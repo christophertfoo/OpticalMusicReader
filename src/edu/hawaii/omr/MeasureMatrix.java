33378eff6c5f80b16659ba073e14b840144b0a3a
Christopher T. Foo <christopher.t.foo@gmail.com>
2013-12-07 13:20:26 -1000

    Added a more infrastructure for the note detection.

--- src/edu/hawaii/omr/MeasureMatrix.java
+++ src/edu/hawaii/omr/MeasureMatrix.java
@@ -1,5 +1,7 @@
 package edu.hawaii.omr;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
 import org.opencv.core.Mat;
 
 public class MeasureMatrix extends ImageMatrix {
@@ -7,12 +9,32 @@ public class MeasureMatrix extends ImageMatrix {
   private final Staff staff;
   private final StaffInfo info;
   private final int offset;
+  private SortedSet<Point> noteCenters = null;
   
   public MeasureMatrix(Mat matrix, Staff staff, StaffInfo info, int offset) {
     super(matrix);
     this.staff = staff;
     this.info = info;
     this.offset = offset;
+  }
+  
+  public SortedSet<Point> findNoteCenters() {
+    SortedSet<Point> centers = new TreeSet<>(new Point.XComparator());
+    
+    this.noteCenters = centers;
+    return centers;
+  }
+  
+  public void getPitches(StringBuilder builder) {
+    if(this.noteCenters == null) {
+      this.findNoteCenters();
+    }
+    
+    for(Point center : this.noteCenters) {
+      builder.append(' ');
+      builder.append(this.staff.getPitchTreble(center.getX() - this.offset, center.getY(), this.info));
+    }
+    builder.append(" |");
   }
   
 }
