d0380488454249996a1330eaadc5f6810217896e
Jack <catinacan@gmail.com>
2013-12-09 00:49:56 -1000

    Changed wholeNote template a little bit.  Will now generate a template that is an ellipse with a diagonal elliptical hole like a regular note. A lot less false positives now.

--- src/edu/hawaii/omr/WholeNote.java
+++ src/edu/hawaii/omr/WholeNote.java
@@ -1,13 +1,15 @@
 package edu.hawaii.omr;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 
 public class WholeNote extends NoteHead {
 
 	private double angle = 0;
 	private double height;
-	public final double threshold = 0.60;
+	public final double threshold = 0.65;
 	
 	public WholeNote(int xCoordinate, int yCoordinate){
 		super.xCoordinate = xCoordinate;
@@ -32,7 +34,7 @@ public class WholeNote extends NoteHead {
 			
 				double value = (Math.pow((x-height), 2)/Math.pow(height,2)) + 
 						(Math.pow((y+height),2)/Math.pow(height/2.0, 2));
-				if (value < 1 && value > 0.2){
+				if (value < 1){
 					double data[] = {255};
 					template.put(Math.abs(y), x, data);
 				}
@@ -42,6 +44,28 @@ public class WholeNote extends NoteHead {
 				}
 			}
 		}	
+		
+		Mat holeTemplate = new Mat((int)(2*height),  (int) (2*height), CvType.CV_8UC1);
+		double holeAngleRotation = 2*Math.PI/3.0;
+		for(int y=0; y>(2*height*-1); y--){
+			for(int x=0; x< (2*height); x++){
+				double value = (Math.pow((x - height) * Math.cos(holeAngleRotation)
+						+ (y + height) * Math.sin(holeAngleRotation), 2) / Math
+							.pow(height/2.0, 2))
+						+ (Math.pow((x - height) * Math.sin(holeAngleRotation)
+								- (y + height) * Math.cos(holeAngleRotation), 2) / Math
+									.pow(height / 3.0, 2));
+				if (value < 1) {
+					double data[] = { 255 };
+					holeTemplate.put(Math.abs(y), x, data);
+				} else {
+					double data[] = { 0 };
+					holeTemplate.put(Math.abs(y), x, data);
+				}
+			}
+		}
+		
+		Core.bitwise_xor(template, holeTemplate, template);
 		return template;
 	}
 
